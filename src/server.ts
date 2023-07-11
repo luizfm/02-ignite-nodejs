c46aba072ceeb7ff6f27f7b015d5bcc553631016
luizfm <luiz.ado@hotmail.com>
2023-07-10 22:27:10 -0300

    chore: test host on app listen description

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app
-  .listen({ port: env.PORT })
+  .listen({ host: '0.0.0.0', port: env.PORT })
   .then(() => console.log('HTTP Server is running on port 3333'))
